a93cb9ef8deb80a329fa463b7455a4a5649a5f19
Sumanth Shankar <sumanthshankar@SumanthShankar.local>
2018-03-19 10:05:34 -0400

    minor bug fixes

--- src/app/githubapi.service.ts
+++ src/app/githubapi.service.ts
@@ -9,17 +9,9 @@ import { Repository } from './model/repository';
 @Injectable()
 export class GitHubApiService {
 
-
     BASE_URL: String = 'https://api.github.com/search/repositories?';
 
-    repositoryLicense: String;
-
-    repositoryForked: String;
-
-    data: Array<Repository> = [];
-
     constructor(private http: Http) { }
-
 
     getRepositories(searchKeyword: String, stars: number, license: String, fork: boolean) {
         return this.http.get(`${this.BASE_URL}q=${searchKeyword}&stars=${stars}&license=${license}&fork=${fork}`)
